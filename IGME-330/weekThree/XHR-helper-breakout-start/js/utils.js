ea7faa43b52c2533c37a0257b8f693b8d6710921
JDC8127 <jdc8127@rit.edu>
2022-09-06 16:37:16 -0400

    file structure and condensed code

--- IGME-330/weekThree/XHR-helper-breakout-start/js/utils.js
+++
@@ -1,9 +0,0 @@
-const loadTextXHR = (url,callback) => {
-    const xhr = new XMLHttpRequest();
-    xhr.onload = callback;
-    xhr.onerror = e => console.log(`In onerror - HTTP Stats Code = ${e.target.status}`);
-    xhr.open("GET",url);
-    xhr.send();
-};
-
-export {loadTextXHR}
